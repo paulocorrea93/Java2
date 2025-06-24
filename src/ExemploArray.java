9bf10babb33fd213cb2793a52d981a9a8dc22580
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-24 14:09:15 -0400

    exemplo array

---
+++ src/ExemploArray.java
@@ -0,0 +1,16 @@
+public class ExemploArray {
+
+    public static void main(String[]args){
+
+        int[] notas = new int[3];
+        notas[0] = 7;
+        notas[1] = 8;
+        notas[2] = 9;
+
+        System.out.println("Nota 1: " + notas[0]);
+        System.out.println("Nota 2: " + notas[1]);
+        System.out.println("Nota 3: " + notas[2]);
+
+    }
+
+}
