e84d1c33f6947cbf9830f62dfef7841ce36f139a
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-24 08:18:35 -0400

    operações simples

---
+++ src/DadosPessoais.java
@@ -0,0 +1,20 @@
+public class DadosPessoais {
+
+    public static void main(String[]args){
+
+        String nome = "Paulo";
+        int idade = 32;
+        double altura = 1.73;
+        boolean matriculado = true;
+        final String pais = "Brasil";
+
+        System.out.println("nome: " + nome);
+        System.out.println("idade: " + idade);
+        System.out.println("altura: " + altura);
+        System.out.println("matriculado: " + matriculado);
+
+        System.out.println("Olá " + nome + " você está no " + pais);
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
 
-
     public static void main(String[]args){
     String mensagemSimples = "Mensagem simples";
         System.out.println(mensagemSimples);
