9e7d29322f6b08c5fb120cb2fe1c5ed0d1cfa45b
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-24 08:07:01 -0400

    mensagem simples

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
+
     public static void main(String[]args){
-        System.out.println("Olá, mundo.");
+    String mensagemSimples = "Mensagem simples";
+        System.out.println(mensagemSimples);
     }
 
 }
