1c327854ab29c39fc92288048cacfc5bfea6b360
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-24 08:56:51 -0400

    break e continue

--- src/TabuadaFor.java
+++ src/TabuadaFor.java
@@ -1,11 +1,17 @@
 public class TabuadaFor {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         int num1 = 3;
 
-        for(int i = 1; i <=10; i++){
-            System.out.println(num1*i);
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 3) {
+                //continue;
+                break;
+            }
+
+            System.out.println(num1 * i);
         }
 
     }
