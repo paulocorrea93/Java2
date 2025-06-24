7f8b319d1f32e466429c41c78858c3d75da15c57
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-24 08:59:18 -0400

    metodo chamando uma mensagem

---
+++ src/Mensagem.java
@@ -0,0 +1,11 @@
+public class Mensagem {
+
+    public static void mensagem (){
+        System.out.println("Exibindo mensagem");
+    }
+
+    public static void main(String[]args){
+        mensagem();
+    }
+
+}
