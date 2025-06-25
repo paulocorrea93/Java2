a4347129aefe43e844ad23da9aa675340ad2a5b5
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-25 07:30:34 -0400

    calcular soma com foreach

--- src/ArrayMaster.java
+++ src/ArrayMaster.java
@@ -2,14 +2,25 @@ public class ArrayMaster {
 
     public static void main(String[] args) {
 
-        int[] numeros = {2, 4, 7, 8, 11};
+        int[] numeros = {2, 4, 6, 8, 10};
         int[] invertidos = new int[numeros.length];
+        int soma = 0;
 
-        for (int i = 0; i < numeros.length; i++) {
-            invertidos[i] = numeros[numeros.length - 1 - i];
+
+        for(int n : numeros){
+            soma += n;
         }
 
-        System.out.println(java.util.Arrays.toString(invertidos));
+        double media = (double) soma / numeros.length;
+
+        System.out.println(media);
+
+
+//        for (int i = 0; i < numeros.length; i++) {
+//            invertidos[i] = numeros[numeros.length - 1 - i];
+//        }
+//
+//        System.out.println(java.util.Arrays.toString(invertidos));
     }
 
 }
