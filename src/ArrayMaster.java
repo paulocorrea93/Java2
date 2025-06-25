764e22aa3d0fbffabdf9eba9ccef7ffc5ff733f7
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-25 07:33:55 -0400

    procurar elemento no array

--- src/ArrayMaster.java
+++ src/ArrayMaster.java
@@ -2,18 +2,29 @@ public class ArrayMaster {
 
     public static void main(String[] args) {
 
-        int[] numeros = {2, 4, 6, 8, 10};
+        int[] numeros = {2, 4, 6, 4, 77, 10};
         int[] invertidos = new int[numeros.length];
         int soma = 0;
 
+        int procurar = 7;
+        boolean achou = false;
 
         for(int n : numeros){
-            soma += n;
+            if(n == 7){
+                achou = true;
+                break;
+            }
         }
 
-        double media = (double) soma / numeros.length;
+        System.out.println(achou);
 
-        System.out.println(media);
+//        for(int n : numeros){
+//            soma += n;
+//        }
+//
+//        double media = (double) soma / numeros.length;
+//
+//        System.out.println(media);
 
 
 //        for (int i = 0; i < numeros.length; i++) {
