19c974626f7cf5422e32d5ec57554a2d3b582708
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-25 07:41:40 -0400

    aprendendo matriz

---
+++ src/MatrizExemplo.java
@@ -0,0 +1,12 @@
+public class MatrizExemplo {
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        System.out.println(matriz[0][2]);
+    }
+
+}
