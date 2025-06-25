b4d1373468b2a9dba348125b7d90b0363b28e3c9
PAULO CORREA <paulocorreapc93@gmail.com>
2025-06-25 08:13:34 -0400

    percorrendo matriz com for

---
+++ src/ExerciciosMatriz01.java
@@ -0,0 +1,24 @@
+public class ExerciciosMatriz01 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+        //        int[][] matriz01 =
+//                {
+//                        {12, 23, 34},
+//                        {45, 56, 67}
+//                };
+//
+//        System.out.println(matriz01[0][0]);
+//        System.out.println(matriz01[0][1]);
+//        System.out.println(matriz01[0][2]);
+//        System.out.println(matriz01[1][0]);
+//        System.out.println(matriz01[1][1]);
+//        System.out.println(matriz01[1][2]);
+    }
+
+
+}
---
+++ src/MatrizComFor.java
@@ -0,0 +1,16 @@
+public class MatrizComFor {
+
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 2, 3},{4, 5, 6}
+        };
+
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                System.out.println("[" + i +"][" + j + "]" + matriz[i][j]);
+            }
+        }
+
+    }
+
+}
